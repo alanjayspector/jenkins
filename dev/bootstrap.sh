14e13f79f4f098720c5d8c724ba6ee9c374e8cd7
Alan Spector <github@aj5.org>
2015-09-16 22:58:30 -0700

    yet more setup changes

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -6,5 +6,5 @@ sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 cd /home/vagrant/app
-npm install
+npm install --no-bin-links
 
